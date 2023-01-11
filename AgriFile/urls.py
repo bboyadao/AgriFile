b650130f23aae93f8056a364ba493a0de652d932
BBOYADAO <bboyadao@gmail.com>
2023-01-12 04:13:56 +0700

    fix conflicts

--- AgriFile/urls.py
+++ AgriFile/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.shortcuts import render
 from django.urls import path, include
-from AgriFile.views import index, Login
-from django.conf import settings
-from django.conf.urls.static import static
+from AgriFile.views import Login
+
+
+def index(request):
+	context = {}
+	return render(request, 'adminlte/index.html', context)
+
 
 urlpatterns = [
-    # path('', index),
-    path('login/', Login.as_view()),
-    path('admin/', admin.site.urls),
-    path('user/', include("user.urls")),
-    path('file/', include("file.urls")),
+	path('', index),
+	path('login/', Login.as_view()),
+	path('admin/', admin.site.urls),
+	path('user/', include("user.urls")),
+	path('file/', include("file.urls")),
 ]
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- AgriFile/views.py
+++ AgriFile/views.py
@@ -1,15 +1,8 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
-from django.urls import reverse_lazy
 
 
 class Login(LoginView):
 	template_name = "user/login.html"
-
-
-def index(request):
-	context = {}
-	return render(request, 'adminlte/index.html', context)
 
 
 class Zone(object):
