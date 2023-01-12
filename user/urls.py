bd4f020fe27b819a50311e5477cb0f17ebf7432d
BBOYADAO <bboyadao@gmail.com>
2023-01-12 12:46:55 +0700

    xong

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from user.views import ListUser, CreateUser, DetailUser, UpdateUser
+from user.views import ListUser, CreateUser, DetailUser, UpdateUser, ChangePassUser
 
 urlpatterns = [
     path('', ListUser.as_view(), name="user_list"),
     path('create/', CreateUser.as_view(), name="user_create"),
     path('<str:slug>/', DetailUser.as_view(), name="user_detail"),
     path('<slug:slug>/update/', UpdateUser.as_view(), name="update_detail"),
-
-
+    path('<slug:slug>/change_pass/', ChangePassUser.as_view(), name="update_detail"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,13 @@
 from django.contrib import messages
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordContextMixin
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, FormView
 
 from user.forms import UserForm, UserUpdateForm
 from user.models import User
@@ -30,4 +38,29 @@ class UpdateUser(UpdateView):
 
     def form_valid(self, form):
         messages.success(self.request, "The task was updated successfully.")
+        return super().form_valid(form)
+
+
+class ChangePassUser(PasswordContextMixin, FormView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy("password_change_done")
+    template_name = "user/change_pass.html"
+    title = "Password change"
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(csrf_protect)
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        # Updating the password logs out all other sessions for the user
+        # except the current one.
+        update_session_auth_hash(self.request, form.user)
         return super().form_valid(form)
