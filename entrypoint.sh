07be342425a11e571dee26b4441bbfaaef0b69de
BBOYADAO <bboyadao@gmail.com>
2023-01-15 20:00:11 +0700

    chang gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createcachetable
 echo "startting appication"
-gunicorn -b 0.0.0.0 -p 9000 AgriFile.wsgi:application
+gunicorn --bind 0.0.0.0:9000 --workers 3 AgriFile.wsgi:application
